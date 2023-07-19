d81450a0f3b7668a38439794ddbac3962ef67a5a
gurneet00 <95477028+gurneet00@users.noreply.github.com>
2023-07-19 17:34:22 +0530

    testing

--- T1.js
+++ T1.js
@@ -3,7 +3,6 @@ const http = require("https")
 fs.writeFile("./api3","",function(errr){})
 async function readmyfile(){
   let promise = new Promise(function(resolve,reject){ 
-
     fs.readFile("./api","utf-8",function(err,data){
         resolve(data)
     }) }) 
@@ -11,35 +10,23 @@ async function readmyfile(){
 }
 
 
-
-
 readmyfile().then(function(data){
-
     var links = data.split("\r\n")
  links.forEach(element => {
   return new Promise(function(resolve,reject){
   http.get(element,function(res)
   { let data = ""
-  
      res.on('data', keys => {
-  
       data = data+keys
-      
-       
       //fs.writeFileSync("./api",data)
-  
      })
      res.on("end",()=>{
       console.log(data)
        resolve(data)
-     })
-       
+     })   
   })
-  
  })
-   
   })
-  
 }
 ).then(function(data) {
   console.log(data)
