3ae2abd90d8c42d9ac6de9cafed48098afd6caf6
gurneet00 <95477028+gurneet00@users.noreply.github.com>
2023-07-20 13:13:34 +0530

    assign 5

--- T1.js
+++ T1.js
@@ -3,7 +3,6 @@ const http = require("https")
 fs.writeFile("./api3","",function(errr){})
 async function readmyfile(){
   let promise = new Promise(function(resolve,reject){ 
-
     fs.readFile("./api","utf-8",function(err,data){
         resolve(data)
     }) }) 
@@ -11,38 +10,42 @@ async function readmyfile(){
 }
 
 
-
-
-readmyfile().then(function(data){
-
+readmyfile().then(
+  function(data){
     var links = data.split("\r\n")
- links.forEach(element => {
-  return new Promise(function(resolve,reject){
-  http.get(element,function(res)
-  { let data = ""
-  
-     res.on('data', keys => {
-  
-      data = data+keys
-      
+    // return new Promise( (resolve, reject) =>{
+    //   resolve("hellooo")
+    // } )
+    var arr=[];
+    console.log(links.length)
+    for(var i = 0 ; i< links.length ; i++ ){
+
+
+     
+     var pro2= new Promise(function(resolve,reject){
+        http.get(links[i],function(res)
+        { let data = ""
+           res.on('data', keys => {
+            data = data+keys
+            //fs.writeFileSync("./api",data)
+           })
+           res.on("end",()=>{
+             resolve(data)
+           })   
+        })
+       })
        
-      //fs.writeFileSync("./api",data)
-  
-     })
-     res.on("end",()=>{
-      console.log(data)
-       resolve(data)
-     })
+       arr.push(pro2)
        
-  })
-  
- })
    
-  })
-  
+      }
+   return Promise.all(arr)
 }
 ).then(function(data) {
-  console.log(data)
-  fs.appendFile("./api3",data+"\n",function(err){})
+for(var files=0;files<data.length;files++){
+  fs.writeFile("./apidata"+files,data[files],function(err){})
+}
   
+}).catch((err)=>{
+  console.log("err")
 });
